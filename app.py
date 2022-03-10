8a13ae8e30b0e6e9945f44bb6674f49c3324edae
gus15378 <92408350+gus15378@users.noreply.github.com>
2022-03-09 22:45:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,83 @@ Base.prepare(engine, reflect=True)
 Measurement = Base.classes.measurement
 Stations = Base.classes.station
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return (
+    "Welcome to the Climate App</br>"
+    "You can click any of the following: </br>"
+    f"/api/v1.0/precipitation</br>"
+    f"/api/v1.0/stations</br>"
+    f"/api/v1.0/tobs</br>"
+    f"/api/v1.0/start</br>"
+    f"/api/v1.0/start/end</br>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    
+    precipitationlist = []
+
+    for date, prcp in results:
+        dict = {}
+        dict['date'] = date
+        dict['precipitation'] = prcp
+        precipitationlist.append(dict)
+
+    session.close()
+
+    return jsonify(precipitationlist)
+    
+@app.route("/api/v1.0/stations")
+def stations():
+    
+    session = Session(engine)
+    results = session.query(Measurement.station).distinct().all()
+    
+    StationList = list(np.ravel(results))
+
+    session.close()
+
+    return jsonify(StationList)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    session = Session(engine)
+    
+    active = session.query(Measurement.station, func.count(Measurement.station)).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+
+    most_active = active[0][0]
+    
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station==most_active).filter(Measurement.date >= '2016-08-24').all()
+
+    TemperatureList = []
+    for date, tobs in results:
+        dict = {}
+        dict['date'] = date
+        dict['temperature'] = tobs
+        TemperatureList.append(dict)
+
+    session.close()
+
+    return jsonify(TemperatureList)
+
+
+
+# @app.route("/api/v1.0/<start>")
+# def start(start):
+
+
+
+# @app.route("/api/v1.0/<start>/<end>")
+# def start_end(start, end):
+    
+            
+
+if __name__ == "__main__":
+    app.run(debug=True)
